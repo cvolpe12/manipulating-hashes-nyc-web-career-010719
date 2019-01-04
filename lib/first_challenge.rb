0ac9acaed3662cb264843f7ce56e620aed3c66b5
Christopher Volpe <christopher.volpe12@gmail.com>
2019-01-04 00:53:36 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -22,7 +22,7 @@ def first_challenge
 
       if attribute == :favorite_icecream_flavors
         value.each do |flavors|
-          flavors.delete_if {|flavor| flavor == "strawberry"}
+          flavors.delete_if? {|flavor| flavor == "strawberry"}
         end
       end
     end
