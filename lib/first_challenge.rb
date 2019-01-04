199afa31e34ce26257c65d6a8f260925865c5323
Christopher Volpe <christopher.volpe12@gmail.com>
2019-01-04 00:53:20 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -2,7 +2,7 @@ def first_challenge
   contacts = {
     "Jon Snow" => {
       name: "Jon",
-      email: "jon_snow@thewall.we", 
+      email: "jon_snow@thewall.we",
       favorite_icecream_flavors: ["chocolate", "vanilla", "mint chip"],
       knows: nil
     },
@@ -16,7 +16,7 @@ def first_challenge
   #your code here
   contacts.each do |person, data|
     puts "#{person}: #{data}"
-  
+
     data.each do |attribute, value|
       puts "#{attribute}: #{data}"
 
@@ -30,4 +30,3 @@ def first_challenge
   #remember to return your newly altered contacts hash!
   contacts
 end
-
